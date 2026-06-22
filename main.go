7ee65a1e6d5f6018e69ad1eabbb509072adf69a7
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-22 16:24:47 +0800

    feat: listen and response to port 8080

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	mylisterer, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer mylisterer.Close()
+
+	fmt.Println("Listening to 8080")
+
+	for {
+		conn, err := mylisterer.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go func(myconnection net.Conn) {
+			defer myconnection.Close()
+
+			response := "HTTP/1.1 200 OK\r\n" +
+				"Content-Length: 22\r\n" +
+				"\r\n" +
+				"Hello from Go server!\n"
+
+			myconnection.Write([]byte(response))
+		}(conn)
+	}
 }
