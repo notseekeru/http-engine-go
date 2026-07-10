9975a5756de6b0140e9ec2e73cf97a85cdb449ae
Stephen Macabulos <myteseeker18@gmail.com>
2026-07-10 15:18:08 +0800

    feat: add keep-alive loop with Connection: close break

--- main.go
+++ main.go
@@ -33,79 +33,83 @@ func main() {
 }
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	conn.SetDeadline(time.Now().Add(5 * time.Second))
-
-	headerHashmap := make(map[string]string)
 	reader := bufio.NewReader(conn)
 
-	requestLine, err := reader.ReadString('\n')
-	if err != nil {
-		return
-	}
-	println()
-	print("Request Line: ", requestLine)
-
-	requestLine = strings.TrimRight(requestLine, "\r\n")
-	requestParts := strings.Split(requestLine, " ")
-
-	if len(requestParts) != 3 {
-		MyHTTPMessage(conn, "400", "Bad Request", "Too many")
-		return
-	}
-	if requestParts[2] != "HTTP/1.1" {
-		MyHTTPMessage(conn, "400", "Bad Request", "Only HTTP/1.1 supported")
-		return
-	}
-	if requestParts[0] != "GET" && requestParts[0] != "POST" {
-		MyHTTPMessage(conn, "405", "Method Not Allowed", "Unsupported HTTP Method")
-		return
-	}
-
 	for {
-		headerLine, err := reader.ReadString('\n')
+		conn.SetDeadline(time.Now().Add(5 * time.Second))
+
+		headerHashmap := make(map[string]string)
+
+		requestLine, err := reader.ReadString('\n')
 		if err != nil {
-			println(err)
 			return
 		}
-		headerLine = strings.TrimRight(headerLine, "\r\n")
-		if headerLine == "" {
-			println("--End of Header--")
-			break
+		println()
+		print("Request Line: ", requestLine)
+
+		requestLine = strings.TrimRight(requestLine, "\r\n")
+		requestParts := strings.Split(requestLine, " ")
+
+		if len(requestParts) != 3 {
+			MyHTTPMessage(conn, "400", "Bad Request", "Too many")
+			return
 		}
-		result := strings.Split(headerLine, ": ")
-		headerHashmap[result[0]] = result[1]
-		fmt.Printf("Header line: %q\n", headerLine)
-	}
-
-	if value, ok := headerHashmap["Content-Length"]; ok {
-		fmt.Println("Content-Length Value:", value)
-		strValue := headerHashmap["Content-Length"]
-		intValue64, err := strconv.ParseInt(strValue, 10, 64)
-		if err != nil {
-			fmt.Printf("PARSING ERROR: Could not convert string %q to int: %v\n", strValue, err)
+		if requestParts[2] != "HTTP/1.1" {
+			MyHTTPMessage(conn, "400", "Bad Request", "Only HTTP/1.1 supported")
+			return
+		}
+		if requestParts[0] != "GET" && requestParts[0] != "POST" {
+			MyHTTPMessage(conn, "405", "Method Not Allowed", "Unsupported HTTP Method")
+			return
 		}
 
-		bodyReader := io.LimitReader(reader, intValue64)
-		bodyBytes, err := io.ReadAll(bodyReader)
-		if err != nil {
-			panic(err)
+		for {
+			headerLine, err := reader.ReadString('\n')
+			if err != nil {
+				println(err)
+				return
+			}
+			headerLine = strings.TrimRight(headerLine, "\r\n")
+			if headerLine == "" {
+				println("--End of Header--")
+				break
+			}
+			result := strings.Split(headerLine, ": ")
+			headerHashmap[result[0]] = result[1]
+			fmt.Printf("Header line: %q\n", headerLine)
 		}
-		fmt.Printf("HTTP Body payload: %s\n", string(bodyBytes))
 
-	} else {
-		fmt.Println("INF: No HTTP Body payload found")
-	}
+		if value, ok := headerHashmap["Content-Length"]; ok {
+			fmt.Println("Content-Length Value:", value)
+			strValue := headerHashmap["Content-Length"]
+			intValue64, err := strconv.ParseInt(strValue, 10, 64)
+			if err != nil {
+				fmt.Printf("PARSING ERROR: Could not convert string %q to int: %v\n", strValue, err)
+			}
 
-	switch requestParts[1] {
-	case "/":
-		MyHTTPMessage(conn, "200", "OK", "index.html File Sent", "html")
-		return
-	case "/ping":
-		MyHTTPMessage(conn, "200", "OK", "pong")
-		return
-	default:
-		MyHTTPMessage(conn, "404", "Not Found", "Not Found")
-		return
+			bodyReader := io.LimitReader(reader, intValue64)
+			bodyBytes, err := io.ReadAll(bodyReader)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("HTTP Body payload: %s\n", string(bodyBytes))
+
+		} else {
+			fmt.Println("INF: No HTTP Body payload found")
+		}
+
+		switch requestParts[1] {
+		case "/":
+			MyHTTPMessage(conn, "200", "OK", "index.html File Sent", "html")
+		case "/ping":
+			MyHTTPMessage(conn, "200", "OK", "pong")
+		default:
+			MyHTTPMessage(conn, "404", "Not Found", "Not Found")
+		}
+
+		if strings.ToLower(headerHashmap["Connection"]) == "close" {
+			return
+		}
 	}
 }
 
