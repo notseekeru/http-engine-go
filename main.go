bdcd167802c24bbe521394f88967c96489ed6d9e
Stephen Macabulos <myteseeker18@gmail.com>
2026-07-15 22:16:05 +0800

    ref(main): simplify main function and remove unused networking code

--- main.go
+++ main.go
@@ -1,85 +1,31 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-	"strconv"
+	"log"
+	"os"
+	"path/filepath"
 	"strings"
-	"time"
 )
 
 // Testing grounds for my implementation of solutions
 
 func main() {
-	myListener, err := net.Listen("tcp", ":8080")
+	maliciousPath := "/index.html"
+	cleanPath := filepath.Clean(maliciousPath)
+	log.Println(cleanPath)
 
+	cwd, err := os.Getwd()
+	log.Println("Current working directory:", cwd)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	defer myListener.Close()
+	fullPath := filepath.Join(cwd, cleanPath)
+	log.Println("Full path:", fullPath)
 
-	for {
-		conn, err := myListener.Accept()
-		if err != nil {
-			println(err)
-			break
-		}
-
-		go func(myConnection net.Conn) {
-			defer myConnection.Close()
-			myConnection.SetDeadline(time.Now().Add(5 * time.Second))
-
-			reader := bufio.NewReader(myConnection)
-
-			requestLine, err := reader.ReadString('\n')
-			if err != nil {
-				return
-			}
-			print("first line: ", requestLine)
-
-			headers := make(map[string]string)
-
-			for {
-				headerLine, err := reader.ReadString('\n')
-				if err != nil {
-					return
-				}
-				headerLine = strings.TrimRight(headerLine, "\r\n")
-				if headerLine == "" {
-					println("End")
-					break
-				}
-				result := strings.Split(headerLine, ": ")
-				headers[result[0]] = result[1]
-				fmt.Printf("Read line: %q\n", headerLine)
-			}
-
-			if value, ok := headers["Content-Length"]; ok {
-				fmt.Println("Found value:", value)
-			} else {
-				fmt.Println("Key not found")
-				return
-			}
-
-			strValue := headers["Content-Length"]
-			intValue64, err := strconv.ParseInt(strValue, 10, 64)
-			if err != nil {
-				fmt.Printf("PARSING ERROR: Could not convert string %q to int: %v\n", strValue, err)
-			}
-			nextByte, _ := reader.Peek(1)
-			fmt.Printf("Cursor is currently resting on character: %q\n", string(nextByte))
-			bodyReader := io.LimitReader(reader, intValue64)
-
-			bodyBytes, err := io.ReadAll(bodyReader)
-			if err != nil {
-				fmt.Printf("CRITICAL ERROR DURING READ: %v\n", err)
-			}
-			fmt.Printf("Body payload read via io: %s\n", string(bodyBytes))
-
-		}(conn)
-
+	if !strings.HasPrefix(fullPath, cwd) {
+		log.Fatal("Access to files outside the allowed directory is not permitted")
 	}
+
+	// Dynamically build your base folder path
 }
