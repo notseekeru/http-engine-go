26061c7e164ef3a7847a6d30a5705d9d36412c5c
Stephen Macabulos <myteseeker18@gmail.com>
2026-07-16 09:12:36 +0800

    ref(main): remove maliciousPath variable and streamline path safety checks

--- main.go
+++ main.go
@@ -9,25 +9,34 @@ import (
 
 func main() {
 	// Test this with "/index.html", "/etc/passwd", and "/../etc/passwd"
-	maliciousPath := "/malicious_shortcut/passwd"
-	log.Println("Malicious path:", maliciousPath)
-
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("The -Cwd-:", cwd)
 
-	fullPath := filepath.Join(cwd, maliciousPath)
-	log.Println("Full path:", fullPath)
+	isSafePath(cwd, "/index.html")
+	isSafePath(cwd, "/etc/passwd")
+	isSafePath(cwd, "/../etc/passwd")
+	isSafePath(cwd, "../http-engine-go-secrets/flag.txt")
+}
 
+func isSafePath(cwd string, targetPath string) bool {
+	filePath := targetPath
+	log.Println("Target path:", filePath)
+
+	fullPath := filepath.Join(cwd, filePath)
+	log.Println("Full path:", fullPath)
 
 	basePrefix := cwd + string(filepath.Separator)
 	log.Println("Base prefix:", basePrefix)
 
 	if !strings.HasPrefix(fullPath, basePrefix) && fullPath != cwd {
-		log.Fatal("Access to files outside the allowed directory is not permitted")
+		log.Println("Error: File path is outside the current working directory.")
+		return false
 	}
 
 	log.Println("Success! File path is safe to use.")
+	log.Println()
+	return true
 }
